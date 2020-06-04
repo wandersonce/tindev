96d66e417143af0a4834c7e39de27708221e2d93
Wanderson Castro <wandersonce1@hotmail.com>
2020-06-03 21:26:37 -0700

    Dislikes and Database filter

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,22 @@ const axios = require('axios');
 const Dev = require('../model/Dev');
 
 module.exports = {
+    async index(req, res) {
+        const { user } = req.headers;
+
+        const loggedDev = await Dev.findById(user);
+
+        const users = await Dev.find({
+            $and: [ //* $and is used to apply more than one condition on the db
+                { _id: { $ne: user } }, // !Search all users that _id is not equal to user
+                { _id: { $nin: loggedDev.likes } }, // !Search all users that _id inside a list (likes)
+                { _id: { $nin: loggedDev.dislikes } } // !Search all users that _id inside a list (dislikes)
+            ],
+        })
+
+        return res.json(users)
+    },
+
     async store(req, res) {
         const { username } = req.body;
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
 const LikeControler = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store)
+
 routes.post('/devs/:devId/likes', LikeControler.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
