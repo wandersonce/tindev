4b402a05549060132126db1df66eea8eecac9b31
Wanderson Castro <wandersonce1@hotmail.com>
2020-06-03 21:13:18 -0700

    Like Controller created

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeControler = require('./controllers/LikeController');
 
 const routes = express.Router();
 
 routes.post('/devs', DevController.store)
+routes.post('/devs/:devId/likes', LikeControler.store);
 
 module.exports = routes;
