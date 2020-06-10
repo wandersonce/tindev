aab5fd215ba3bd44c06072c9455934f7986c00f3
Wanderson Castro <wandersonce1@hotmail.com>
2020-06-09 21:56:29 -0700

    Routes created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './App.css';
 
-import Login from './pages/Login/Login';
+import Routes from './routes';
 
 function App() {
   return (
-    <Login />
+    <Routes />
   );
 }
 
