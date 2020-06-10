1c22a6f81066908deda08cfa8bb3fe4da8ce1e2e
Wanderson Castro <wandersonce1@hotmail.com>
2020-06-09 22:16:04 -0700

    Axios connected with backend

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import logo from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 import './Login.css';
 
-export default function Login() {
+export default function Login({ history }) {
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username,
+        });
+
+        console.log(response);
+
+        history.push('/main')
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="tindev logo" />
-                <input type="text" placeholder="Github username" />
+                <input
+                    type="text"
+                    placeholder="Github username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
                 <button type="submit">Send</button>
             </form>
         </div>
