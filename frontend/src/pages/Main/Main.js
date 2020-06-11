134e3856a1dfaed85af0d68056e4dd4889fb540e
Wanderson Castro <wandersonce1@hotmail.com>
2020-06-10 21:23:46 -0700

    Main state and usereffect working

--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import like from '../../assets/like.svg';
@@ -7,58 +9,43 @@ import dislike from '../../assets/dislike.svg';
 import './Main.css';
 
 export default function Main({ match }) { //* inside the match you will have all parameters that was sent to this route
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('devs', {
+                headers: {
+                    user: match.params.id
+                },
+            })
+
+            setUsers(response.data);
+        }
+        loadUsers();
+    }, [match.params.id]) // * Everytime that the params id be changed the function will be executed.
+
     return (
         <div className="main-container">
             <img src={logo} alt="Tindev" />
             <ul>
-                <li>
-                    <img src="https://avatars0.githubusercontent.com/u/51806895?v=4" alt="Profile image" />
-                    <footer>
-                        <strong>Wanderson Castro</strong>
-                        <p>Web developer, formed by BCIT, Canada. Passion for solve problem solving and Information ​​Technology development. Always learning more !</p>
-                    </footer>
+                {users.map(user => (
+                    <li key={user._id}>
+                        <img src={user.avatar} alt={user.name} />
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars0.githubusercontent.com/u/51806895?v=4" alt="Profile image" />
-                    <footer>
-                        <strong>Wanderson Castro</strong>
-                        <p>Web developer, formed by BCIT, Canada. Passion for solve problem solving and Information ​​Technology development. Always learning more !</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars0.githubusercontent.com/u/51806895?v=4" alt="Profile image" />
-                    <footer>
-                        <strong>Wanderson Castro</strong>
-                        <p>Web developer, formed by BCIT, Canada. Passion for solve problem solving and Information ​​Technology development. Always learning more !</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={like} alt="Like" />
+                            </button>
+                            <button type="button">
+                                <img src={dislike} alt="Dislike" />
+                            </button>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     )
